62f753c0e9ee00955704407aafbb24a174fbf9f0
Uzair Ishaq <uzair.i@hotmail.co.uk>
2018-03-03 23:20:08 +0000

    good version

--- app/src/Components/Footer/index.js
+++ app/src/Components/Footer/index.js
@@ -7,20 +7,20 @@ class Footer extends Component {
   render() {
     var take=[];
     if(this.props.cofr>50 || this.props.Conditions == "rain"){
-        take.push(<img src={require('../../Images/umbrella.png')} height="50px" width="50px"/>);
+        take.push(<img src={require('../../Images/umbrella.png')} height="40px" width="40px"/>);
     }
     if(this.props.Temperature<14 || this.props.low<10){
-      take.push(<img src={require('../../Images/jacket.png')} height="50px" width="50px"/>);
+      take.push(<img src={require('../../Images/jacket.png')} height="40px" width="40px"/>);
       if(this.props.Temperature<8 || this.props.low<4){
-        take.push(<img src={require('../../Images/hat.png')} height="50px" width="50px"/>);
-        take.push(<img src={require('../../Images/scarf.png')} height="50px" width="50px"/>);
+        take.push(<img src={require('../../Images/hat.png')} height="40px" width="40px"/>);
+        take.push(<img src={require('../../Images/scarf.png')} height="40px" width="40px"/>);
       }
     }
     else if(this.props.Temperature>20 || this.props.high>25){
-      take.push(<img src={require('../../Images/suncream.png')} height="50px" width="50px"/>);
+      take.push(<img src={require('../../Images/suncream.png')} height="40px" width="40px"/>);
     }
     if(this.props.Conditions == "sunny" ){
-        take.push(<img src={require('../../Images/sunglasses.png')} height="50px" width="50px"/>);
+        take.push(<img src={require('../../Images/sunglasses.png')} height="40px" width="40px"/>);
     }
     return (
       <div className="footer">
