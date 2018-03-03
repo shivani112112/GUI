5bde30974029b8424096fef90f070dd5e842f69f
Rhiannaa Singh <rhiannaasingh22@gmail.com>
2018-03-03 19:42:53 +0000

    CSS changes
    made some changes for css

--- app/src/Components/Conditions/index.js
+++ app/src/Components/Conditions/index.js
@@ -9,16 +9,16 @@ class Conditions extends Component {
   render() {
     return (
     <div className="conditions">
-    <div className="conditions.l">
-        <div className="conditions.l.emoji">EMOJI</div>
-        <div className="conditions.l.cond"><p>{this.props.condition}</p></div>
+    <div className="conditions_l">
+        <div className="conditions_l_emoji">EMOJI</div>
+        <div className="conditions_l_cond"><p>{this.props.condition}</p></div>
     </div>
 
-      <div className="conditions.r">
-          <div className="conditions.r.hi"><p>High:{this.props.hi}</p></div>
-          <div className="conditions.r.temp"><p>Current Temperature:{this.props.temperature}</p></div>
-          <div className="conditions.r.lo"><p>Lo:{this.props.lo}</p></div>
-          <div className="conditions.r.fl"><p>Feelslike: {this.props.feelslike}</p></div>
+      <div className="conditions_r">
+          <div className="conditions_r_hilo"><p>High:{this.props.hi}</p></div>
+          <div className="conditions_r_temp"><p>Current Temperature:{this.props.temperature}</p></div>
+          <div className="conditions_r_hilo"><p>Lo:{this.props.lo}</p></div>
+          <div className="conditions_r_fl"><p>Feelslike: {this.props.feelslike}</p></div>
       </div>
 
     </div>
