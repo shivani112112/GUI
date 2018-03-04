0dd0c22ac19052f61b5fd8ec5e91f187eb47df58
Gobihan <gobihan1997@gmail.com>
2018-03-04 21:46:36 +0000

    Hi guys

--- app/src/App.js
+++ app/src/App.js
@@ -29,45 +29,89 @@ class App extends Component {
       hours:[],
       icons:[],
       isSnackbarActive: true,
-      search:""
+      search1:""
     };
     this.setLocation.bind(this)
   }
   getLocation = () =>{
     //var long = this.state.locationLong;
-    console.log(this.state.search)
-    var url="http://api.wunderground.com/api/1671bd6891040bea/conditions/q/"+this.state.locationLat+","+this.state.locationLong+".json"
-    console.log(url)
-  $.ajax({
-    url: url,
-    dataType: "jsonp",
-    success : this.parseConditions,
-    error : function(req, err){ console.log('API call failed ' + err); }
-  })
-  $.ajax({
-    url: "http://api.wunderground.com/api/1671bd6891040bea/yesterday/q/"+this.state.locationLat+","+this.state.locationLong+".json",
-    dataType: "jsonp",
-    success : this.parseYesterday,
-    error : function(req, err){ console.log('API call failed ' + err); }
-  })
+    //var long = this.state.locationLong;
+      //console.log(this.state.locationLat
+      console.log(document.getElementById('search').value);
+        if(this.state.search1=="") console.log("gobi is moist");
 
-  $.ajax({
-    url: "http://api.wunderground.com/api/1671bd6891040bea/forecast10day/q/"+this.state.locationLat+","+this.state.locationLong+".json",
-    dataType: "jsonp",
-    success : this.parseForecast,
-    error : function(req, err){ console.log('API call failed ' + err); }
-  })
-  $.ajax({
-    url: "http://api.wunderground.com/api/1671bd6891040bea/planner_07010731/q/"+this.state.locationLat+","+this.state.locationLong+".json",
-    dataType: "jsonp",
-    success : this.parseRain,
-    error : function(req, err){ console.log('API call failed ' + err); }
-  })
+
+      if(this.state.search1!=""){
+  	   var url="http://api.wunderground.com/api/1671bd6891040bea/conditions/q/UK/"+this.state.search1+".json"
+      console.log(url)
+    $.ajax({
+      url: url,
+      dataType: "jsonp",
+      success : this.parseConditions,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/yesterday/q/UK/"+this.state.search1+".json",
+      dataType: "jsonp",
+      success : this.parseYesterday,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/forecast10day/q/UK/"+this.state.search1+".json",
+      dataType: "jsonp",
+      success : this.parseForecast,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/planner_07010731/q/UK/"+this.state.search1+".json",
+      dataType: "jsonp",
+      success : this.parseRain,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+  	}
+  	else{
+  		var url="http://api.wunderground.com/api/1671bd6891040bea/conditions/q/"+this.state.locationLat+","+this.state.locationLong+".json"
+      console.log(url)
+    $.ajax({
+      url: url,
+      dataType: "jsonp",
+      success : this.parseConditions,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/yesterday/q/"+this.state.locationLat+","+this.state.locationLong+".json",
+      dataType: "jsonp",
+      success : this.parseYesterday,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/forecast10day/q/"+this.state.locationLat+","+this.state.locationLong+".json",
+      dataType: "jsonp",
+      success : this.parseForecast,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+    $.ajax({
+      url: "http://api.wunderground.com/api/1671bd6891040bea/planner_07010731/q/"+this.state.locationLat+","+this.state.locationLong+".json",
+      dataType: "jsonp",
+      success : this.parseRain,
+      error : function(req, err){ console.log('API call failed ' + err); }
+    })
+  	}
 }
 
-setLocation = (theSearch) => {
-  console.log(theSearch);
-  this.setState({search:theSearch});
+setLocation = () => {
+  var temp = document.getElementById('search').value;
+  temp.replace(" ","_");
+  this.setState({search1: temp}
+,() => {
+  console.log(this.state.search1);
+  this.getLocation();
+});
+
+  this.render();
+
 
 }
 
@@ -81,8 +125,17 @@ setLocation = (theSearch) => {
     return (
       <div className="App">
 
-        <Header location={this.state.location} search={this.setLocation}/>
-        <Conditions temperature={this.state.temperature} condition={this.state.condition} feelslike={this.state.feelslike} hi={this.state.hi} lo={this.state.lo}/>
+      <div className="header">
+          <div id="header_dropdown">
+
+              <input  name="uzair" id="search"  />
+              <button onClick={this.setLocation}> Name </button>
+
+          </div>
+          <div className="header_search"><button onClick={this.changeLocation.bind(this)}><img src={require('./Images/search.png')} height="20 px" width="20 px"/></button></div>
+            <div className="header_location"><p align="center">{this.state.location}</p></div>
+      </div>
+        <Conditions temperature={this.state.temperature} condition={this.state.location} feelslike={this.state.feelslike} hi={this.state.hi} lo={this.state.lo}/>
         <Rest/>
           <Compare yHi={this.state.yesterdayHi} yLo={this.state.yesterdayLo} high={this.state.hi} low={this.state.lo}/>
         <Footer Conditions={this.state.condition} Temperature={this.state.temperature} high={this.state.hi} low={this.state.lo} cofr={this.state.rain}/>
@@ -96,9 +149,11 @@ setLocation = (theSearch) => {
   }
   success = (pos) => {
   var crd = pos.coords;
-  this.setState({locationLat:crd.latitude, locationLong:crd.longitude}
+  this.setState({locationLat:crd.latitude, locationLong:crd.longitude, search:this.state.locationLat+","+this.state.locationLong}
     ,() => {
       if(this.state.flag){
+        var temp=this.state.locationLat+","+this.state.locationLong;
+        console.log(this.state.search)
         this.getLocation()
         this.state.flag=false
     }
@@ -109,6 +164,12 @@ setLocation = (theSearch) => {
   error = (err) => {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 };
+
+
+changeLocation=(e)=> {
+    document.getElementById("header_dropdown").style.height="10%";
+    document.getElementById("header_dropdown").style.top="0%";
+  }
 
 handleShowSnackbar() {
   this.setState({
