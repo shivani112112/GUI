36e83c24f6e6c1fc05cf3108a665243c2579e831
Shivani <shivanipatel112@hotmail.com>
2018-03-04 15:07:57 +0000

    Hi guys

--- app/src/Components/Header/index.js
+++ app/src/Components/Header/index.js
@@ -12,7 +12,7 @@ class Header extends Component {
 
       <div className="header">
           <button onClick={this.changeLocation.bind(this)}><div className="header_search"><img src={require('../../Images/search.png')} height="20 px" width="20 px"/></div></button>
-            <div className="header_location"><p align="center">{this.props.location}</p></div>
+          <div className="header_location"><p align="center">{this.props.location}</p></div>
       </div>
 
     );
