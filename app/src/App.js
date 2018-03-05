a22cb2dac5983d02db2bbbf0848beb5ba3170d06
Uzair Ishaq <uzair.i@hotmail.co.uk>
2018-03-05 01:50:56 +0000

    nice search

--- app/src/App.js
+++ app/src/App.js
@@ -131,7 +131,7 @@ document.getElementById("header_dropdown").style.top="-40%";
           <div id="header_dropdown">
 
               <input  name="uzair" id="search"  />
-              <button onClick={this.setLocation}> Search </button>
+              <button onClick={this.setLocation}> Go </button>
 
           </div>
           <div className="header_search"><button onClick={this.changeLocation.bind(this)}><img src={require('./Images/search.png')} height="20 px" width="20 px"/></button></div>
@@ -169,7 +169,7 @@ document.getElementById("header_dropdown").style.top="-40%";
 
 
 changeLocation=(e)=> {
-    document.getElementById("header_dropdown").style.height="7%";
+    document.getElementById("header_dropdown").style.height="5%";
     document.getElementById("header_dropdown").style.top="0%";
   }
 
