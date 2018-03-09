5febaa377858237b790c7e27902bb0f0de0ac4e1
Gobihan <gobihan1997@gmail.com>
2018-03-09 12:29:42 +0000

    Fix

---
+++ app/src - Copy/Components/Compare/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Compare extends Component {
+
+  render() {
+    var max="yesterday";
+      if(this.props.yHi>this.props.high && this.props.yLo> this.props.low) max="cooler than " +max;
+      else if(this.props.yHi<this.props.high && this.props.yLo< this.props.low) max="warmer than "+ max;
+      else max="same as " + max;
+
+    return (
+      <div className="compare">
+          <p>{max}</p>
+      </div>
+    );
+  }
+}
+export default Compare;
---
+++ app/src - Copy/Components/Rest/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+class Rest extends Component {
+
+	render() {
+		var hourcells=[];
+		var iconcells=[];
+<<<<<<< HEAD
+<<<<<<< HEAD
+		//b,c,e,F
+		if(this.props.hours==undefined){
+			
+		}else{
+			console.log(this.props.icons);
+			for(var i=0; i<this.props.hours.length ;i++){
+			hourcells.push(<td>{this.props.hours[i]}</td>);
+			var temp = this.props.icons[i]
+			var p = 'sunface';
+						   
+						   
+			if(temp==="Clear"  || temp==="Very Hot" || temp==="Mostly Sunny" || temp==="Sunny") {
+				var d= new Date();
+				var n= d.getHours();
+				if(n>this.props.sunset||n<this.props.sunrise) p="moon2";
+				else p ="sunface";
+		  }
+			  else if(temp=== "Partly Cloudy" || temp ==="Mostly Cloudy"|| temp==="Scattered Clouds" || temp==="Partly Sunny") p="partcloudy";
+			  else if(temp==="Cloudy" || temp==="Overcast") p = "cloudy";
+			  else if(temp==="Very Cold") p = "cold";
+			  else if(temp==="Chance of Flurries" || temp==="Chance of Freezing Rain" || temp==="Chance of Sleet" || temp==="Chance of Snow" || temp==="Freezing Rain" || temp==="Sleet" || temp==="Snow" || temp==="Flurries") p = "snow";
+			  else if(temp==="Chance of Rain" || temp==="Chance Rain" || temp==="Rain") p = "rain";
+			  else if(temp==="Chance of Thunderstorms" || temp==="Chance of a Thunderstorm" || temp==="Thunderstorm" || temp==="Thunderstorms" || temp==="Unknown") p = "thunder";
+			  else if(temp==="Fog"|| temp==="Haze") p="fog";	   
+						   
+						   
+						   
+						   
+						   
+						   
+			iconcells.push(<td><img src={require('../../Images/'+p+'.png')} width="40px" height="40px" /></td>);
+		}
+		}
+=======
+=======
+>>>>>>> 08b74b1b4db1312c5a712806bb7de2931c11d917
+			if(this.props.hours===undefined){}
+			else{
+					for(var i=0; i<this.props.hours.length ;i++){
+							hourcells.push(<td>{this.props.hours[i]}</td>);
+							var temp = this.props.icons[i]
+							var p = 'sunface';
+								if(temp==="Clear"  || temp==="Very Hot" || temp==="Mostly Sunny" || temp==="Sunny") {
+									var d= new Date();
+									var n= d.getHours();
+										if(n>this.props.sunset||n<this.props.sunrise) p="newmoon";
+										else p ="sunface";
+		  					}
+			  				else if(temp=== "Partly Cloudy" || temp ==="Mostly Cloudy"|| temp==="Scattered Clouds" || temp==="Partly Sunny") p="partcloudy";
+			  				else if(temp==="Cloudy" || temp==="Overcast") p = "cloudy";
+			  				else if(temp==="Very Cold") p = "cold";
+			  				else if(temp==="Chance of Flurries" || temp==="Chance of Freezing Rain" || temp==="Chance of Sleet" || temp==="Chance of Snow" || temp==="Freezing Rain" || temp==="Sleet" || temp==="Snow" || temp==="Flurries") p = "snow";
+			  				else if(temp==="Chance of Rain" || temp==="Chance Rain" || temp==="Rain") p = "rain";
+			  				else if(temp==="Chance of Thunderstorms" || temp==="Chance of a Thunderstorm" || temp==="Thunderstorm" || temp==="Thunderstorms" || temp==="Unknown") p = "thunder";
+			  				else if(temp==="Fog"|| temp==="Haze") p="fog";
+							iconcells.push(<td><img src={require('../../Images/'+p+'.png')} alt={p} width="40px" height="40px" /></td>);
+					}
+			}
+<<<<<<< HEAD
+>>>>>>> 08b74b1b4db1312c5a712806bb7de2931c11d917
+=======
+>>>>>>> 08b74b1b4db1312c5a712806bb7de2931c11d917
+		return (
+			<div className="rest">
+				<table>
+					<tbody>
+						<tr>
+							{hourcells}
+						</tr>
+						<tr>
+							{iconcells}
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+}
+export default Rest;
